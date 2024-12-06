584c97a2074267f48dae492501c37090906cde77
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-12-06 08:08:15 -0300

    feat: add initial boilerplate

---
+++ src/main/java/com/nanapapais/names_autocomplete/NamesAutocompleteApplication.java
@@ -0,0 +1,13 @@
+package com.nanapapais.names_autocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamesAutocompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamesAutocompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanapapais/names_autocomplete/NamesAutocompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanapapais.names_autocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamesAutocompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
